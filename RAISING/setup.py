b9f649ca363312d626ac4ddbb65cb1975062a436
Devashish Tripathi <devashishtripathi697@gmail.com>
2024-10-09 01:41:20 +0530

    Add files via upload

---
+++ RAISING/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hp_tuning',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your Python package dependencies here
+    ],
+)
+
