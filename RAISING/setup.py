42a95d21555c474bae7c72c82490f24324ff6285
Devashish Tripathi <devashishtripathi697@gmail.com>
2024-10-09 02:00:03 +0530

    Update setup.py

--- RAISING/setup.py
+++ RAISING/setup.py
@@ -8,13 +8,15 @@ setup(
     description='RAISING:A supervised deep learning framework for hyperparameter tuning and feature selection',
     packages=find_packages(),
     install_requires=[
-        "pandas>=1.5.2",
+       "pandas>=1.5.2",
         "numpy>=1.23.5",
         "tensorflow>=2.11.0",
         "keras>=2.11.0",
         "scikit-learn>=1.0.2",
         "keras-tuner>=1.1.3",
         "pysnptools>=0.5.10",
-        "tensorflow_addons>=0.18.0"
+        "tensorflow_addons>=0.18.0",
+        "imbalanced-learn>=0.11.0",
+        "shap>=0.41.0"
     ],
 )
